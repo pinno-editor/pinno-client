742ce38bc499b30fc17ab094f3085854e036f6be
Vasily Sviridov <vasily-sviridov@eduprog.ru>
2022-09-22 00:14:36 +0300

    feat: create VideoLoader component

---
+++ components/VideoLoader/VideoLoader.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import VideoLoader from "./VideoLoader";
+
+export default {
+  title: "VideoLoader",
+  component: VideoLoader,
+};
+
+export const Primary = () => <VideoLoader></VideoLoader>;
---
+++ components/VideoLoader/VideoLoaderProps.ts
@@ -0,0 +1,4 @@
+export enum VideoLoaderState {
+  kUnloaded,
+  kLoaded,
+}
