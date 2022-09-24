4f5c119f77b313c35e23a97a91c7806b87f61ccb
Константин Орлов <konstantinorlov@MacBook-Pro-Konstantin.local>
2022-09-24 16:11:11 +0300

    update

--- components/VideoGrid/VideoGrid.tsx
+++ components/VideoGrid/VideoGrid.tsx
@@ -15,13 +15,12 @@ const VideoGrid = () => {
     useEffect(() => {
         if (fakeData) {
             setVideos([
-                { title: "first", tile: "https://kartinkin.net/uploads/posts/2022-03/1646311456_37-kartinkin-net-p-kartinki-loshadok-44.jpg", link: "link", id:"1" },
-                { title: "first", tile: "https://s1.1zoom.ru/big3/421/USA_Rivers_Scenery_Grand_450593.jpg", link: "link",id:"2" }
+                { title: "first", tile: "https://kartinkin.net/uploads/posts/2022-03/1646311456_37-kartinkin-net-p-kartinki-loshadok-44.jpg", link: "link", id: "1" },
+                { title: "first", tile: "https://s1.1zoom.ru/big3/421/USA_Rivers_Scenery_Grand_450593.jpg", link: "link", id: "2" }
             ]);
         }
     }, [])
 
-<<<<<<< HEAD
     if (videos.length == 0) { return <div className={styles.video_loader_wrapper}><VideoLoader></VideoLoader></div>; }
     return (
         <Container maxWidth="lg">
@@ -31,20 +30,7 @@ const VideoGrid = () => {
                         <VideoItem video={video}></VideoItem>
                     </Grid>
                 ))}
-            </Grid>
-=======
-
-    return (
-        <Container maxWidth="lg">
-                <Grid container spacing={1} >
-                    {videos.map((video: Video, id) => (
-                        <Grid key={id} item sm={4} xs={4} md={4}>
-                            <VideoItem video={video}></VideoItem>
-                        </Grid>
-                    ))}
-                </Grid>
->>>>>>> 19cd75bae90ec5ef68c38a34f223afc7125b8bba
-            </Container>
-            )
+            </Grid></Container>
+    )
 }
-            export default VideoGrid;
+export default VideoGrid;
